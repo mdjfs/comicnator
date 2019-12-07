c62b2cdefacf8d96c2e4ad2e8d195b5a2f3e3c41
Wolfang Torres <wolfang.torres@gmail.com>
2019-12-07 01:51:43 -0400

    final fix

--- comicnator/static/js/imagescell.js
+++ comicnator/static/js/imagescell.js
@@ -4,6 +4,7 @@ if (!(resultado.indexOf("tu personaje") >= 0)) {
     resultado = resultado.substring(24, resultado.length);
     resultado = resultado.toLowerCase();
     resultado = resultado.replace(/ /g, "");
+    resultado = resultado.substring(1, resultado.length)
     document.getElementById("personaje").src = url_for_static(resultado + ".jpg")
     document.getElementById("final").innerHTML = "No es el personaje que pensabas?";
     document.getElementById("irfinal").value = "Dime en quien pensabas";
