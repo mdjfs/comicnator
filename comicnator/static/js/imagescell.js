2ac658883e9ed9c5240a8875a2951f2fcd181525
mdjfs <56895143+mdjfs@users.noreply.github.com>
2019-12-08 22:21:56 -0400

    arreglo para que muestre contenido en telefonos

--- comicnator/static/js/imagescell.js
+++ comicnator/static/js/imagescell.js
@@ -1,11 +1,10 @@
 var resultado = document.getElementById('1').innerHTML;
 
 if (!(resultado.indexOf("tu personaje") >= 0)) {
-    resultado = resultado.substring(24, resultado.length);
+    resultado = resultado.substring(16, resultado.length);
     resultado = resultado.toLowerCase();
     resultado = resultado.replace(/ /g, "");
-    resultado = resultado.substring(1, resultado.length)
-    document.getElementById("personaje").src = url_for_static(resultado + ".jpg")
+    document.getElementById("personaje").src = url_for_static(resultado + ".jpg");
     document.getElementById("final").innerHTML = "No es el personaje que pensabas?";
     document.getElementById("irfinal").value = "Dime en quien pensabas";
 }
@@ -14,5 +13,5 @@ else {
     document.getElementById("irfinal").value = "Dime en quien pensabas";
 }
 function imagenotfound() {
-    document.getElementById("personaje").src = url_for_static("notfound.jpg")
+    document.getElementById("personaje").src = url_for_static("notfound.jpg");
 }
